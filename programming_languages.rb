54244070804febadf0f7a196ec7586ec6a222ae2
Omar A <oazzat19@gmail.com>
2018-10-19 00:37:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
+  new_hash = {}
   
+  languages.each do |key,val|
+    
+    val.each do |name,type|
+      if !new_hash.include?(name)
+        new_hash[name] = type
+      end
+    end 
+    
+  end
+  puts new_hash
 end
